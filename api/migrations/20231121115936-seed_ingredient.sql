a3b6a3487ae4f98e127de59ce00dde3f2acca126
toshiyuki <148667226+toshi-arch@users.noreply.github.com>
2023-11-28 23:59:58 +0900

    マイグレーションファイル作成ver.2

--- api/migrations/20231121115936-seed_ingredient.sql
+++ api/migrations/20231121115936-seed_ingredient.sql
@@ -1,41 +1,44 @@
 
 -- +migrate Up
 INSERT INTO ingredients (name, type, alcohol)
-VALUES("ドライジン", 1, 40)
+VALUES("ドライジン", 1, 40);
 
 INSERT INTO ingredients (name, type, alcohol)
-VALUES("ウォッカ", 1, 40)
+VALUES("ウォッカ", 1, 40);
 
 INSERT INTO ingredients (name, type, alcohol)
-VALUES("ウィスキー", 1, 40)
+VALUES("ウィスキー", 1, 40);
 
 INSERT INTO ingredients (name, type, alcohol)
-VALUES("ラム",1, 40)
+VALUES("ラム",1, 40);
 
 INSERT INTO ingredients (name, type, alcohol)
-VALUES("トニックウォーター", 6, 0)
+VALUES("トニックウォーター", 6, 0);
 
 INSERT INTO ingredients (name, type, alcohol)
-VALUES("ジンジャーエール", 6, 0)
+VALUES("ジンジャーエール", 6, 0);
 
 INSERT INTO ingredients (name, type, alcohol)
-VALUES("ソーダ", 6, 0)
+VALUES("ソーダ", 6, 0);
 
 INSERT INTO ingredients (name, type, alcohol)
-VALUES("コアントロー", 2, 40)
+VALUES("コアントロー", 2, 40);
 
 INSERT INTO ingredients (name, type, alcohol)
-VALUES("レモンジュース", 6, 0)
+VALUES("レモンジュース", 6, 0);
 
 INSERT INTO ingredients (name, type, alcohol)
-VALUES("アマレット", 2, 24)
+VALUES("アマレット", 2, 24);
 
 INSERT INTO ingredients (name, type, alcohol)
-VALUES("ドライベルモット", 4, 17)
+VALUES("ドライベルモット", 4, 17);
 
 INSERT INTO ingredients (name, type, alcohol)
-VALUES("カンパリ", 2, 25)
+VALUES("カンパリ", 2, 25);
 
 INSERT INTO ingredients (name, type, alcohol)
-VALUES("グレナデンシロップ", 6, 0)
+VALUES("グレナデンシロップ", 6, 0);
+
+INSERT INTO ingredients (name, type, alcohol)
+VALUES("ライムジュース", 6, 0);
 -- +migrate Down
--- api/migrations/20231121124611-create_ingredients_cocktails.sql
+++ api/migrations/20231121124611-create_ingredients_cocktails.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS ingredients_cocktails (
     id int NOT NULL AUTO_INCREMENT,
     ingredient_id int NOT NULL,
-    cocktails_id int NOT NULL,
+    cocktail_id int NOT NULL,
     amount int NOT NULL,
     unit int NOT NULL,
     created_at datetime NOT NULL default CURRENT_TIMESTAMP,
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS ingredients_cocktails (
 
     PRIMARY KEY(id),
     FOREIGN KEY(ingredient_id) REFERENCES ingredients(id),
-    FOREIGN KEY(cocktails_id) REFERENCES cocktails(id)
+    FOREIGN KEY(cocktail_id) REFERENCES cocktails(id)
 );
 -- +migrate Down
-DROP TABLE IF EXISTS ingredients_cocktails
+DROP TABLE IF EXISTS ingredients_cocktails;
