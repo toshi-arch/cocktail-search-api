b5324525074e25f50d32d034e9c9d670963dc892
toshiyuki <148667226+toshi-arch@users.noreply.github.com>
2023-11-30 21:10:09 +0900

    cocktailsとingredientsのseedファイル訂正

--- api/migrations/20231120124959-seed_cocktails.sql
+++ api/migrations/20231120124959-seed_cocktails.sql
@@ -1,26 +1,12 @@
 
 -- +migrate Up
-INSERT INTO cocktails(name, alcohol, recipe)
-VALUES("ジントニック", 12, "省略1");
-
-INSERT INTO cocktails(name, alcohol, recipe)
-VALUES("ジンバック", 12, "省略2");
-
-INSERT INTO cocktails(name, alcohol, recipe)
-VALUES("ウォッカトニック", 12, "省略3");
-
-INSERT INTO cocktails(name, alcohol, recipe)
-VALUES("モスコミュール", 12, "省略4");
-
-INSERT INTO cocktails(name, alcohol, recipe)
-VALUES("ハイボール", 12, "省略5");
-
-INSERT INTO cocktails(name, alcohol, recipe)
-VALUES("XYZ", 25, "省略6");
-
-INSERT INTO cocktails(name, alcohol, recipe)
-VALUES("フリッパー", 9, "省略7");
-
-INSERT INTO cocktails(name, alcohol, recipe)
-VALUES("シャーリーテンプル", 0, "省略8");
+INSERT INTO cocktails(name, alcohol, recipe)VALUES
+("ジントニック", 12, "省略1"),
+("ジンバック", 12, "省略2"),
+("ウォッカトニック", 12, "省略3"),
+("モスコミュール", 12, "省略4"),
+("ハイボール", 12, "省略5"),
+("XYZ", 25, "省略6"),
+("フリッパー", 9, "省略7"),
+("シャーリーテンプル", 0, "省略8");
 -- +migrate Down
--- api/migrations/20231121115936-seed_ingredient.sql
+++ api/migrations/20231121115936-seed_ingredient.sql
@@ -1,44 +1,18 @@
 
 -- +migrate Up
-INSERT INTO ingredients (name, type, alcohol)
-VALUES("ドライジン", 1, 40);
-
-INSERT INTO ingredients (name, type, alcohol)
-VALUES("ウォッカ", 1, 40);
-
-INSERT INTO ingredients (name, type, alcohol)
-VALUES("ウィスキー", 1, 40);
-
-INSERT INTO ingredients (name, type, alcohol)
-VALUES("ラム",1, 40);
-
-INSERT INTO ingredients (name, type, alcohol)
-VALUES("トニックウォーター", 6, 0);
-
-INSERT INTO ingredients (name, type, alcohol)
-VALUES("ジンジャーエール", 6, 0);
-
-INSERT INTO ingredients (name, type, alcohol)
-VALUES("ソーダ", 6, 0);
-
-INSERT INTO ingredients (name, type, alcohol)
-VALUES("コアントロー", 2, 40);
-
-INSERT INTO ingredients (name, type, alcohol)
-VALUES("レモンジュース", 6, 0);
-
-INSERT INTO ingredients (name, type, alcohol)
-VALUES("アマレット", 2, 24);
-
-INSERT INTO ingredients (name, type, alcohol)
-VALUES("ドライベルモット", 4, 17);
-
-INSERT INTO ingredients (name, type, alcohol)
-VALUES("カンパリ", 2, 25);
-
-INSERT INTO ingredients (name, type, alcohol)
-VALUES("グレナデンシロップ", 6, 0);
-
-INSERT INTO ingredients (name, type, alcohol)
-VALUES("ライムジュース", 6, 0);
+INSERT INTO ingredients (name, type, alcohol)VALUES
+("ドライジン", 1, 40),
+("ウォッカ", 1, 40),
+("ウィスキー", 1, 40),
+("ラム",1, 40),
+("トニックウォーター", 6, 0),
+("ジンジャーエール", 6, 0),
+("ソーダ", 6, 0),
+("コアントロー", 2, 40),
+("レモンジュース", 6, 0),
+("アマレット", 2, 24),
+("ドライベルモット", 4, 17),
+("カンパリ", 2, 25),
+("グレナデンシロップ", 6, 0),
+("ライムジュース", 6, 0);
 -- +migrate Down
